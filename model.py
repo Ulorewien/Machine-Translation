ba974b5b496232efb2c35d175ee0db43f4d8ee51
Ulorewien <rohanshingre7@gmail.com>
2024-06-15 13:49:42 -0700

    Implemented decoder

--- model.py
+++ model.py
@@ -12,3 +12,14 @@ class Encoder(nn.Module):
         for layer in self.layers:
             x = layer(x, mask)
         return self.layernorm(x)
+    
+class Decoder(nn.Module):
+    def __init__(self, layers):
+        super().__init__()
+        self.layers = layers
+        self.layernorm = LayerNormalization()
+
+    def forward(self, x, encoder_output, source_mask, target_mask):
+        for layer in self.layers:
+            x = layer(x, encoder_output, source_mask, target_mask)
+        return self.layernorm(x)
