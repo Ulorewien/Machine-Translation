f9aff3619eb82b491c66adbf4b2532589547e097
Ulorewien <rohanshingre7@gmail.com>
2024-06-15 13:57:53 -0700

    Implemented Transformer

--- model.py
+++ model.py
@@ -23,3 +23,32 @@ class Decoder(nn.Module):
         for layer in self.layers:
             x = layer(x, encoder_output, source_mask, target_mask)
         return self.layernorm(x)
+    
+class Transformer(nn.Module):
+    def __init__(self, encoder, decoder, source_embd, target_embd, source_pos, target_pos, projection_layer):
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.source_embd = source_embd
+        self.target_embd = target_embd
+        self.source_pos = source_pos
+        self.target_pos = target_pos
+        self.projection_layer = projection_layer
+
+    def encode(self, x, source_mask):
+        x = self.source_embd(x)
+        x = self.source_pos(x)
+        x = self.encoder(x)
+        return x
+    
+    def decode(self, encoder_output, source_mask, x, target_mask):
+        x = self.target_embd(x)
+        x = self.target_pos(x)
+        x = self.decoder(x, encoder_output, source_mask, target_mask)
+        return x
+    
+    def project(self, x):
+        return self.projection_layer(x)
+    
+def buildTransformer():
+    
