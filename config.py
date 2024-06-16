892f61b48288d4b653176e596753182e60d586b5
Ulorewien <rohanshingre7@gmail.com>
2024-06-15 17:22:00 -0700

    Updated config

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ def get_config():
         "n_epochs": 20,
         "lr": 1e-4,
         "seq_len": 350,
-        "d_model": 512,
+        "d_model": 64,
         "language_source": "en",
         "language_target": "it",
         "model_folder": "weights",
@@ -12,4 +12,8 @@ def get_config():
         "preload": None,
         "tokenizer_file": "tokenizer_{0}.json",
         "experiment_name": "runs/model",
+        "n_layer": 4,
+        "n_heads": 2,
+        "dropout": 0.1,
+        "d_ff": 256,
     }
